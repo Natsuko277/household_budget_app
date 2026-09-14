1ad617a521f534e62e4b195a694939d6702918a4
Natsuko277 <kanno.natsuko@group-ath.com>
2026-09-14 16:49:40 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kakeibo-cache-v4';
+const CACHE_NAME = 'kakeibo-cache-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -38,4 +38,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
